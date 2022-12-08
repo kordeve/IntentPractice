ef267a2c2708bed2949929c5ce58936ac74670d1
SHI <shinxx21@gmail.com>
2022-12-08 13:19:49 +0900

    Initial commit

---
+++ app/src/main/java/com/emptymind/intentpractice/OtherActivity.kt
@@ -0,0 +1,18 @@
+package com.emptymind.intentpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+        returnToMainBtn.setOnClickListener {
+
+            finish()
+
+        }
+    }
+}
