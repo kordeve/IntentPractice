c94bd28e2d798ccc658289e6c3fef4a1a1005b89
SHI <shinxx21@gmail.com>
2022-12-14 02:40:23 +0900

    결과를 받아 돌아온 화면에서, 새 닉네임 반영

--- app/src/main/java/com/emptymind/intentpractice/EditContentActivity.kt
+++ app/src/main/java/com/emptymind/intentpractice/EditContentActivity.kt
@@ -1,11 +1,36 @@
 package com.emptymind.intentpractice
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_content.*
+
 
 class EditContentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_content)
+
+
+        okBtn.setOnClickListener {
+
+//            입력한 새 닉네임이 뭔지? 변수로 저장.
+            val inputNewNickname = newNicknameEdt.text.toString()
+
+//            입력한 닉네임을 가지고 메인으로 복귀.
+
+//            왕복티켓처럼 이전 화면 Intent에서 정보를 입력했으므로 지금 화면 Intent()에는 돌아가기 위한 정보를 입력할 필요가 없다.
+//            입력닉네임을 담아주기 위한 용도로만 사용하는 Intent.
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickname",inputNewNickname)
+            setResult(Activity.RESULT_OK,resultIntent)
+//            OK버튼 눌림과, 백버튼 (취소) 눌림을 구별해야한다.
+            finish()
+
+
+
+        }
+
     }
 }
--- app/src/main/java/com/emptymind/intentpractice/MainActivity.kt
+++ app/src/main/java/com/emptymind/intentpractice/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.emptymind.intentpractice
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    val REQURST_FOR_NICKNAME = 1005
+    //    멤버변수는 class 중괄호 바로 앞에 써 주는 변수
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sendMessageBtn.setOnClickListener {
+
+            val inputMessage = edtMessage.text.toString()
+
+            val myIntent = Intent(this, viewMessageActivity::class.java)
+
+            myIntent.putExtra("sendMessage", inputMessage)
+
+            startActivity(myIntent)
+
+        }
+
+        editNicknameBtn.setOnClickListener {
+
+            val myIntent = Intent(this, EditContentActivity::class.java)
+            startActivityForResult(myIntent, REQURST_FOR_NICKNAME)
+        }
 
         moveToOtherBtn.setOnClickListener {
 
@@ -18,19 +43,29 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        sendMessageBtn.setOnClickListener {
 
-            val inputMessage = edtMessage.text.toString()
+    }
 
-            val myIntent = Intent(this, viewMessageActivity::class.java)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-            myIntent.putExtra("sendMessage",inputMessage)
+        //돌아온 이유가 닉네임을 받으러 다녀온게 맞는지?
+        if(requestCode == REQURST_FOR_NICKNAME){
 
-            startActivity(myIntent)
+            //추가질문: 확인을 눌러서 돌아온게 맞는가?
+            if(resultCode == Activity.RESULT_OK){
 
+                //실제 첨부된 새 닉네임을 꺼내서 텍스트뷰에 반영
+                val newNickname = data?.getStringExtra("nickname")
+                nickName.text = newNickname
+
+            }
 
         }
+
     }
+
 }
+
 
 // 변수이름 myIntent가 중복되도 정상적 실행.
